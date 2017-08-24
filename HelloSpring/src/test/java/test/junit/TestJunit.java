065cbabb4ab7af07eb9daa62e49748323b5645f5
csmz-sugy <sugy@chinamobilesz.com>
2017-08-24 14:37:24 +0800

    add junit

---
+++ HelloSpring/src/test/java/test/junit/TestJunit.java
@@ -0,0 +1,16 @@
+package test.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestJunit {
+	
+	
+	@Test
+	public void test() {
+		
+		 Assert.assertEquals(3, 3);
+		
+	}
+
+}
